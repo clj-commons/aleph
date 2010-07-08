98d21978c928196e064a56ccde39ed136e2f631e
datskos <datskosgeorge@gmail.com>
2010-07-08 19:44:59 +0900

    bump Netty version to 3.2.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject aleph "0.1.0-SNAPSHOT"
   :description "an asynchronous web server"
-  :repositories [["JBoss" "http://repository.jboss.org/maven2"]]
+  :repositories [["JBoss" "http://repository.jboss.org/nexus/content/groups/public/"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [autodoc "0.7.1"]]
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-				 [org.jboss.netty/netty "3.2.0.BETA1"]])
+                 [org.jboss.netty/netty "3.2.1.Final"]])
