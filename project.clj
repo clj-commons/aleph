c898ef693cf5b82bb3d37f933625b1f28fdd94e2
Zachary Tellman <ztellman@gmail.com>
2010-08-07 11:15:13 -0700

    update push method

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   :description "an asynchronous web server"
   :repositories [["jboss" "http://repository.jboss.org/nexus/content/groups/public/"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [autodoc "0.7.1"]
-					 [lein-clojars "0.5.0-SNAPSHOT"]]
+                     [autodoc "0.7.1"]]
   :dependencies [[org.clojure/clojure "1.2.0-beta1"] 
 				 [org.clojure/clojure-contrib "1.2.0-beta1"]
                  [org.jboss.netty/netty "3.2.1.Final"]])
