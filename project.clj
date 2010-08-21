302c90a1a200dd10e545811d6d0cdb0d319d6fff
prospero <ztellman@gmail.com>
2010-08-21 14:03:15 -0700

    repository list needs to be a map, apparently

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject aleph "0.1.0-SNAPSHOT"
   :description "an asynchronous web server"
-  :repositories [["jboss" "http://repository.jboss.org/nexus/content/groups/public/"]]
+  :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"}
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [autodoc "0.7.1"]]
   :dependencies [[org.clojure/clojure "1.2.0"] 
