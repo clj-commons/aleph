11d2006eeeca08f27804f5b3b5126083c62d9a2e
Matthew Maravillas <matthew.maravillas@m33integrated.com>
2011-02-27 15:38:03 -0500

    When converting data (i.e. request bodies) to channel buffers, accept byte arrays

--- src/aleph/formats.clj
+++ src/aleph/formats.clj
@@ -10,6 +10,8 @@
   aleph.formats
   (:use
     [clojure.contrib.json])
+  (:require
+    [clj-http.util :as http-util])
   (:import
     [java.io
      InputStream
@@ -87,19 +89,25 @@
   ([s charset]
      (-> s (string->byte-buffer charset) byte-buffer->channel-buffer)))
 
+(defn byte-array? [data]
+  (and (.isArray (class data))
+       (= (.getComponentType (class data)) Byte/TYPE)))
+
 (defn to-channel-buffer [data]
   (cond
     (instance? ChannelBuffer data) data
     (instance? ByteBuffer data) (byte-buffer->channel-buffer data)
     (instance? InputStream data) (input-stream->channel-buffer data)
-    (instance? String data) (string->channel-buffer data)))
+    (instance? String data) (string->channel-buffer data)
+    (byte-array? data) (string->channel-buffer (http-util/utf8-string data))))
 
 (defn to-channel-buffer? [data]
   (or
     (instance? ChannelBuffer data)
     (instance? ByteBuffer data)
     (instance? InputStream data)
-    (instance? String data)))
+    (instance? String data)
+    (byte-array? data)))
 
 ;;;
 
