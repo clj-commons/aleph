3f0767ce96bb45d456e103a0aa5ae32b5c2eeccb
ztellman <ztellman@gmail.com>
2011-05-22 21:24:27 -0700

    update version to 0.2.0-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.1.5-SNAPSHOT"
+(defproject aleph "0.2.0-alpha1"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"}
   :license {:name "Eclipse Public License - v 1.0"
@@ -7,8 +7,8 @@
   :dependencies [[org.clojure/clojure "1.2.0"] 
                  [org.jboss.netty/netty "3.2.4.Final"]
                  [clj-http "0.1.0-SNAPSHOT"]
-                 [lamina "0.4.0-SNAPSHOT"]
-                 [gloss "0.1.2-SNAPSHOT"]
+                 [lamina "0.4.0-alpha1"]
+                 [gloss "0.2.0-alpha1"]
                  [user-agent-utils "1.2.3"]
                  [potemkin "0.1.0"]]
   :jvm-opts ["-server"])
