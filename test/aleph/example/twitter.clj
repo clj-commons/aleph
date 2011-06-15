23255d9239fb1adfc82b8ede08792b76823e18d5
ztellman <ztellman@gmail.com>
2011-06-14 17:23:54 -0700

    add duration parameter to twitter example

--- test/aleph/example/twitter.clj
+++ test/aleph/example/twitter.clj
@@ -17,15 +17,18 @@
 (def username "aleph_example")
 (def password "_password")
 
-(defn sample-stream []
-  (let [req (sync-http-request
-	      {:method :get
-	       :basic-auth [username password]
-	       :url "http://stream.twitter.com/1/statuses/sample.json"
-	       :auto-transform true})]
-    (Thread/sleep 500)
-    (close (:body req))
-    req))
+(defn sample-stream
+  ([]
+     (sample-stream 500))
+  ([duration]
+     (let [req (sync-http-request
+		 {:method :get
+		  :basic-auth [username password]
+		  :url "http://stream.twitter.com/1/statuses/sample.json"
+		  :auto-transform true})]
+       (Thread/sleep duration)
+       (close (:body req))
+       req)))
 
 (defn twitter-proxy-handler [ch request]
   ;; since we use the same format for our responses and the ones we receive from
