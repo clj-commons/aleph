3755dd1baa4186faea0fb67873c0fc933581a9dc
ztellman <ztellman@gmail.com>
2011-08-03 13:38:31 -0700

    update to beta2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.2.0-alpha3-SNAPSHOT"
+(defproject aleph "0.2.0-beta2-SNAPSHOT"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"}
   :license {:name "Eclipse Public License - v 1.0"
