43b472c503f7df5837d75ba53a4be11fb3806152
ztellman <ztellman@gmail.com>
2011-11-11 16:24:55 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.2.0-rc1"
+(defproject aleph "0.2.0-rc2"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"}
   :license {:name "Eclipse Public License - v 1.0"
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"] 
                  [org.jboss.netty/netty "3.2.5.Final"]
                  [clj-http "0.1.3"]
-                 [lamina "0.4.0-rc1"]
+                 [lamina "0.4.0-rc2"]
                  [gloss "0.2.0-rc1"]
                  [user-agent-utils "1.2.3"]
                  [potemkin "0.1.0"]]
