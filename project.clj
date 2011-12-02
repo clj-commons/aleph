90877d75b7c0ff2a7001d329ff9288970e9975ad
ztellman <ztellman@gmail.com>
2011-12-02 15:18:39 -0800

    fix project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(aleph "0.2.1-SNAPSHOT"
+(defproject aleph "0.2.1-SNAPSHOT"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"}
   :license {:name "Eclipse Public License - v 1.0"
