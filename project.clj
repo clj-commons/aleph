aa55023713c391ffe465611e17d0181845ac7b73
Zach Tellman <ztellman@gmail.com>
2012-01-24 22:40:36 -0800

    bump to 0.2.1-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.2.1-SNAPSHOT"
+(defproject aleph "0.2.1-alpha1"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"}
   :license {:name "Eclipse Public License - v 1.0"
@@ -10,8 +10,8 @@
                  [org.clojure.contrib/prxml "1.3.0-alpha4"]
                  [io.netty/netty "3.3.0.Final"]
                  [clj-http "0.1.3"]
-                 [lamina "0.4.1-SNAPSHOT"]
-                 [gloss "0.2.1-SNAPSHOT"]
+                 [lamina "0.4.1-alpha1"]
+                 [gloss "0.2.1-alpha1"]
                  [user-agent-utils "1.2.3"]
                  [potemkin "0.1.1"]]
   :multi-deps {:all [[org.clojure/tools.logging "0.2.3"]
@@ -19,8 +19,8 @@
                      [org.clojure.contrib/prxml "1.3.0-alpha4"]
                      [io.netty/netty "3.3.0.Final"]
                      [clj-http "0.1.3"]
-                     [lamina "0.4.1-SNAPSHOT"]
-                     [gloss "0.2.1-SNAPSHOT"]
+                     [lamina "0.4.1-alpha1"]
+                     [gloss "0.2.1-alpha1"]
                      [user-agent-utils "1.2.3"]
                      [potemkin "0.1.1"]]
                "1.2" [[org.clojure/clojure "1.2.1"]]}
