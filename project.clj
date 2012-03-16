bb0657f8e1e8192bd62e98d30a18328cf347b6c2
Zach Tellman <ztellman@gmail.com>
2012-03-16 12:59:41 -0700

    back to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.2.1-beta1"
+(defproject aleph "0.2.1-SNAPSHOT"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
