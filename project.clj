26e8407096bb13b8715b81e176328df8cfcd8da5
Zach Tellman <ztellman@gmail.com>
2012-03-31 16:12:50 +0530

    use warning-less prxml, and update to latest lamina to allow compilation

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.2.1-SNAPSHOT"
+(defproject aleph "0.2.1-beta2"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
@@ -8,10 +8,10 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/data.json "0.1.1"]
-                 [org.clojure.contrib/prxml "1.3.0-alpha4"]
+                 [prxml "1.3.1"]
                  [io.netty/netty "3.3.1.Final"]
                  [clj-http "0.1.3"]
-                 [lamina "0.4.1-beta1"]
+                 [lamina "0.4.1-beta2"]
                  [gloss "0.2.1-beta1"]
                  [user-agent-utils "1.2.3"]
                  [potemkin "0.1.2"]]
