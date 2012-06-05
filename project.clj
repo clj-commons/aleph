e758d71afb1844b591b92be8832b5992d64efe87
Zach Tellman <ztellman@gmail.com>
2012-06-05 15:34:25 -0700

    mark 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.2.1-rc4"
+(defproject aleph "0.2.1"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
@@ -11,8 +11,8 @@
                  [prxml "1.3.1"]
                  [io.netty/netty "3.4.2.Final"]
                  [clj-http "0.1.3"]
-                 [lamina "0.4.1-rc2"]
-                 [gloss "0.2.1-rc3"]
+                 [lamina "0.4.1"]
+                 [gloss "0.2.1"]
                  [user-agent-utils "1.2.3"]
                  [potemkin "0.1.3"]]
   :multi-deps {:all [[org.clojure/tools.logging "0.2.3"]
@@ -20,8 +20,8 @@
                      [prxml "1.3.1"]	
                      [io.netty/netty "3.4.2.Final"]
                      [clj-http "0.1.3"]
-                     [lamina "0.4.1-rc2"]
-                     [gloss "0.2.1-rc3"]
+                     [lamina "0.4.1"]
+                     [gloss "0.2.1"]
                      [user-agent-utils "1.2.3"]
                      [potemkin "0.1.3"]]
                "1.2" [[org.clojure/clojure "1.2.1"]]
