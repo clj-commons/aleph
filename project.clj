55acf5efff8b5a1bf944c0eb3d649da47d775aed
Vadim Geshel <vadim@yummly.com>
2012-10-29 11:13:49 -0700

    Restored project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.vgeshel/aleph "0.3.0-SNAPSHOT"
+(defproject aleph "0.3.0-SNAPSHOT"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
