2aaf4446c4f78ea9940a78aefd001d11e5fcdb16
Zach Tellman <ztellman@gmail.com>
2012-12-05 15:20:05 -0800

    fix 'all' profile

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
-  :aliases {"all" ["with-profile" "1.2:1.3:dev:1.5"]}
+  :aliases {"all" ["with-profile" "1.2,dev:1.3,dev:dev:1.5,dev"]}
   :plugins [[codox "0.6.2"]]
   :test-selectors {:default #(not (some #{:benchmark :redis} (cons (:tag %) (keys %))))
                    :integration :redis
