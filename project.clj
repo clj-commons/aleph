7216a660af8773e781e50ea548ef32de961ff0e4
Zach Tellman <ztellman@gmail.com>
2013-03-16 23:29:55 -0700

    mark beta15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.3.0-SNAPSHOT"
+(defproject aleph "0.3.0-beta15"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/tools.logging "0.2.3"]
                  [org.clojure/data.xml "0.0.7"]
                  [io.netty/netty "3.6.2.Final"]
-                 [lamina "0.5.0-SNAPSHOT"]
+                 [lamina "0.5.0-beta14"]
                  [gloss "0.2.2-beta5"]
                  [cheshire "5.0.1"]
                  [commons-codec/commons-codec "1.7"]
