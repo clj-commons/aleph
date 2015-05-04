750c5811f8e9d1d23fab5eaea654f361abc5dea1
Zach Tellman <ztellman@gmail.com>
2015-05-03 18:11:32 -0700

    update example dependencies

--- examples/project.clj
+++ examples/project.clj
@@ -2,7 +2,7 @@
   :dependencies [[aleph "0.4.0"]
                  [manifold "0.1.0"]
                  [gloss "0.2.5"]
-                 [compojure "1.3.1"]
-                 [org.clojure/clojure "1.7.0-beta1"]
+                 [compojure "1.3.3"]
+                 [org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :plugins [[lein-marginalia "0.8.0"]])
