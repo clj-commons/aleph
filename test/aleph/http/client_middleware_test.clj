ad934a7754891e2fcd64f2be661e6d9152ccc204
Ivan Kryvoruchko <gildraug@gmail.com>
2015-11-30 18:23:36 +0200

    Fix HTTP client StringIndexOutOfBoundsException when empty query params are passed

---
+++ test/aleph/http/client_middleware_test.clj
@@ -0,0 +1,8 @@
+(ns aleph.http.client-middleware-test
+  (:require [aleph.http.client-middleware :as middleware]
+            [clojure.test :as t :refer [deftest is]]))
+
+(deftest test-empty-query-string
+  (is (= "" (middleware/generate-query-string {})))
+  (is (= "" (middleware/generate-query-string {} "text/plain; charset=utf-8")))
+  (is (= "" (middleware/generate-query-string {} "text/html;charset=ISO-8859-1"))))
