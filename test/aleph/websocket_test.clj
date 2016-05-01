13336f867f8f665011ffb0585c7450d1b9479ca7
Zach Tellman <ztellman@gmail.com>
2016-04-30 22:19:48 -0700

    increase websocket timeout

--- test/aleph/websocket_test.clj
+++ test/aleph/websocket_test.clj
@@ -37,5 +37,5 @@
   (with-handler echo-handler
     (let [c @(http/websocket-client "ws://localhost:8080")]
       (s/put! c "hello")
-      (is (= "hello" @(s/try-take! c 1000))))
+      (is (= "hello" @(s/try-take! c 5e5))))
     (is (= 400 (:status @(http/get "http://localhost:8080" {:throw-exceptions false}))))))
