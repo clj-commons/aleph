cea561271d791f5176d6da9925a966da476bfe93
Zach Tellman <ztellman@gmail.com>
2017-04-27 20:14:13 -0700

    mark 0.4.4-alpha3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.4.4-alpha2"
+(defproject aleph "0.4.4-alpha3"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
