9dcfc60321700ce271a41fc4765671f6f3bef5eb
Zach Tellman <ztellman@gmail.com>
2017-05-19 15:43:28 -0700

    update netty dependency, mark 0.4.4-alpha4

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject aleph "0.4.4-alpha3"
+(defproject aleph "0.4.4-alpha4"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :license {:name "MIT License"}
   :dependencies [[org.clojure/tools.logging "0.3.1" :exclusions [org.clojure/clojure]]
-                 [io.netty/netty-all "4.1.9.Final"]
+                 [io.netty/netty-all "4.1.11.Final"]
                  [manifold "0.1.6"]
                  [byte-streams "0.2.2"]
                  [potemkin "0.4.3"]]
