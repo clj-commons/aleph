66d64fe56059ea79dc614cf7ed2455f3582f4efb
ztellman <ztellman@gmail.com>
2017-11-05 16:16:28 -0800

    mark 0.4.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.4.4-alpha4"
+(defproject aleph "0.4.4"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
