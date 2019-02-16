2673e8b5de597ec427805e215bd1aff43249e32e
Alexey Kachayev <kachayev@gmail.com>
2019-02-16 01:47:16 +0100

    SNI test not to rely on local DNS configuration

--- test/aleph/sni_test.clj
+++ test/aleph/sni_test.clj
@@ -19,23 +19,29 @@
          default-ssl# (netty/self-signed-ssl-context)]
      (with-server (http/start-server ~handler
                                      {:port port
-                                      :ssl-context {"aleph.local" ssl#
+                                      :ssl-context {"aleph.io.local" ssl#
                                                     "*" default-ssl#}})
        ~@body)))
 
 (deftest test-sni-handler
   (let [pool1 (http/connection-pool
                {:connection-options
+                {:sni {:host "aleph.io.local"}
+                 :ssl-context ssl-test/client-ssl-context}})
+        pool2 (http/connection-pool
+               {:connection-options
                 {:ssl-context ssl-test/client-ssl-context}})]
     (with-sni-handler ok-handler
       (testing "succcess on known host"
-        (let [resp (http/get (str "https://aleph.local:" port)
+        (let [resp (http/get (str "https://aleph.io.local:" port)
                              {:pool pool1})]
           (is (= 200 (:status @resp)))))
 
+      ;; do not specify SNI host manually
       ;; self-signed should not be trusted
       (testing "fails on unrecognized host"
         (is (thrown? Exception
                      @(http/get (str "https://127.0.0.1:" port)
-                                {:pool pool1})))))
-    (.shutdown ^IPool pool1)))
+                                {:pool pool2})))))
+    (.shutdown ^IPool pool1)
+    (.shutdown ^IPool pool2)))
