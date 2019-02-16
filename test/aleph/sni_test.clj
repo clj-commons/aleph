389a182b89d8d3b5b228ae43c3922f2a92e1174b
Alexey Kachayev <kachayev@gmail.com>
2019-02-16 17:01:40 +0100

    Test case to check default SNI host when using static name resolver

--- test/aleph/sni_test.clj
+++ test/aleph/sni_test.clj
@@ -20,21 +20,38 @@
      (with-server (http/start-server ~handler
                                      {:port port
                                       :ssl-context {"aleph.io.local" ssl#
+                                                    "*.netty.io.local" ssl#
                                                     "*" default-ssl#}})
        ~@body)))
 
 (deftest test-sni-handler
   (let [pool1 (http/connection-pool
                {:connection-options
-                {:sni {:host "aleph.io.local"}
+                {:sni {:peer-host "aleph.io.local"}
                  :ssl-context ssl-test/client-ssl-context}})
         pool2 (http/connection-pool
                {:connection-options
+                {:name-resolver (netty/static-name-resolver
+                                 {"aleph.io.local" "127.0.0.1"
+                                  "docs.netty.io.local" "127.0.0.1"})
+                 :ssl-context ssl-test/client-ssl-context}})
+        pool3 (http/connection-pool
+               {:connection-options
                 {:ssl-context ssl-test/client-ssl-context}})]
     (with-sni-handler ok-handler
-      (testing "succcess on known host"
-        (let [resp (http/get (str "https://aleph.io.local:" port)
+      (testing "succcess on manually specified host"
+        (let [resp (http/get (str "https://127.0.0.1:" port)
                              {:pool pool1})]
+          (is (= 200 (:status @resp)))))
+
+      (testing "succcess on resolved host"
+        (let [resp (http/get (str "https://aleph.io.local:" port)
+                             {:pool pool2})]
+          (is (= 200 (:status @resp)))))
+
+      (testing "success on wildcard domain"
+        (let [resp (http/get (str "https://docs.netty.io.local:" port)
+                             {:pool pool2})]
           (is (= 200 (:status @resp)))))
 
       ;; do not specify SNI host manually
@@ -42,6 +59,7 @@
       (testing "fails on unrecognized host"
         (is (thrown? Exception
                      @(http/get (str "https://127.0.0.1:" port)
-                                {:pool pool2})))))
+                                {:pool pool3})))))
     (.shutdown ^IPool pool1)
-    (.shutdown ^IPool pool2)))
+    (.shutdown ^IPool pool2)
+    (.shutdown ^IPool pool3)))
