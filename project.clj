df65e5405d21a74cc8418697f05c74c1147a4efe
Matthew Davidson <matthew@modulolotus.net>
2021-10-30 18:28:31 +0100

    Update .gitignore for clj-kondo and lsp

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
     [potemkin "0.4.5"]])
 
 (defproject aleph "0.4.7-alpha9"
-  :description "a framework for asynchronous communication"
+  :description "A framework for asynchronous communication"
   :repositories {"jboss" "https://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :license {:name "MIT License"}
