83850fb50e8d160eb089a98a38bbe42c892fcb5c
Matthew Davidson <matthew@modulolotus.net>
2021-10-31 22:37:50 +0000

    Remove 1.7-specific java options
    According to the State of Clojure survey, less than half a percent of
    people use Java 7 or earlier, so I'm comfortable requiring 8+

--- project.clj
+++ project.clj
@@ -27,10 +27,10 @@
                    (map
                      #(vector (symbol "io.netty" (str "netty-" %)) netty-version)
                      netty-modules))
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
-                                  [criterium "0.4.6"]
-                                  [cheshire "5.10.0"]
-                                  [org.slf4j/slf4j-simple "1.7.30"]
+  :profiles {:dev  {:dependencies [[org.clojure/clojure "1.10.1"]
+                                   [criterium "0.4.6"]
+                                   [cheshire "5.10.0"]
+                                   [org.slf4j/slf4j-simple "1.7.30"]
                                    [com.cognitect/transit-clj "1.0.324"]]}
              ;; This is for self-generating certs for testing ONLY:
              :test {:dependencies [[org.bouncycastle/bcprov-jdk15on "1.69"]
@@ -48,7 +48,6 @@
             [lein-marginalia "0.9.1"]
             [ztellman/lein-cljfmt "0.1.10"]]
   :java-source-paths ["src/aleph/utils"]
-  :javac-options ["-target" "1.7", "-source" "1.7"]
   :cljfmt {:indents {#".*" [[:inner 0]]}}
   :test-selectors {:default #(not
                                (some #{:benchmark :stress}
