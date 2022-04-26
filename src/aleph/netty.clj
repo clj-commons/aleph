24b43bf6d6c293c24f11aea5d20736b9cd759bce
Arnaud Geiser <arnaudgeiser@gmail.com>
2022-04-26 23:30:56 +0200

    Use DefaultThreadFactory

--- src/aleph/netty.clj
+++ src/aleph/netty.clj
@@ -4,13 +4,9 @@
     [clj-commons.byte-streams :as bs]
     [clojure.tools.logging :as log]
     [manifold.deferred :as d]
-    [manifold.executor :as e]
     [manifold.stream :as s]
     [manifold.stream.core :as manifold]
     [clj-commons.primitive-math :as p]
-    [clojure
-     [string :as str]
-     [set :as set]]
     [potemkin :as potemkin :refer [doit doary]])
   (:import
     [java.io IOException]
@@ -29,7 +25,6 @@
      EpollSocketChannel
      EpollDatagramChannel]
     [io.netty.util Attribute AttributeKey]
-    [io.netty.handler.codec Headers]
     [io.netty.channel.nio NioEventLoopGroup]
     [io.netty.channel.socket ServerSocketChannel]
     [io.netty.channel.socket.nio
@@ -56,7 +51,7 @@
      ResourceLeakDetector$Level]
     [java.net URI SocketAddress InetSocketAddress]
     [io.netty.util.concurrent
-     GenericFutureListener Future]
+     DefaultThreadFactory GenericFutureListener Future]
     [java.io InputStream File]
     [java.nio ByteBuffer]
     [io.netty.util.internal SystemPropertyUtil]
@@ -797,26 +792,18 @@
   (let [cpu-count (->> (Runtime/getRuntime) (.availableProcessors))]
     (max 1 (SystemPropertyUtil/getInt "io.netty.eventLoopThreads" (* cpu-count 2)))))
 
-(defn ^ThreadFactory enumerating-thread-factory [prefix daemon?]
-  (let [num-threads (atom 0)]
-    (e/thread-factory
-     #(str prefix "-" (swap! num-threads inc))
-     (deliver (promise) nil)
-     nil
-     daemon?)))
-
 (def ^String client-event-thread-pool-name "aleph-netty-client-event-pool")
 
 (def epoll-client-group
   (delay
     (let [thread-count (get-default-event-loop-threads)
-          thread-factory (enumerating-thread-factory client-event-thread-pool-name true)]
+          thread-factory (DefaultThreadFactory. client-event-thread-pool-name true)]
       (EpollEventLoopGroup. (long thread-count) thread-factory))))
 
 (def nio-client-group
   (delay
     (let [thread-count (get-default-event-loop-threads)
-          thread-factory (enumerating-thread-factory client-event-thread-pool-name true)]
+          thread-factory (DefaultThreadFactory. client-event-thread-pool-name true)]
       (NioEventLoopGroup. (long thread-count) thread-factory))))
 
 (defn convert-address-types [address-types]
@@ -1006,7 +993,7 @@ initialize an DnsAddressResolverGroup instance.
    epoll?]
   (let [num-cores      (.availableProcessors (Runtime/getRuntime))
         num-threads    (* 2 num-cores)
-        thread-factory (enumerating-thread-factory "aleph-netty-server-event-pool" false)
+        thread-factory (DefaultThreadFactory. "aleph-netty-server-event-pool")
         closed?        (atom false)
 
         ^EventLoopGroup group
