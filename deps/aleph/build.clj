f2c528f44b587b8f18b86e13b3d86af096ed8e44
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2022-09-09 11:58:52 +0200

    Add comment to `deps/aleph/build.clj` to inform about its purpose

--- deps/aleph/build.clj
+++ deps/aleph/build.clj
@@ -1,6 +1,10 @@
 (ns aleph.build
   (:require [clojure.tools.build.api :as b]))
 
+;; WARNING: This is *not* Aleph's official build process. It merely
+;; replicates a subset of the official Leiningen-based build process
+;; to allow Aleph to be used as a git or local root dependency.
+
 (def basis (b/create-basis {:project "deps.edn"}))
 (def build-edn (read-string (slurp "deps/aleph/build.edn")))
 
