7676c4423069f07229881c876b5ffd42d38007b5
Matthew Davidson <matthew@modulolotus.net>
2022-09-20 15:30:28 +0800

    Move clj-http deps to dev profile for ease of dev

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 
 (defproject aleph (or (System/getenv "PROJECT_VERSION") "0.5.0")
   :description "A framework for asynchronous communication"
-  :repositories {"jboss" "https://repository.jboss.org/nexus/content/groups/public/"
+  :repositories {"jboss"               "https://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :url "https://github.com/clj-commons/aleph"
   :license {:name "MIT License"}
@@ -21,47 +21,47 @@
                  [io.netty/netty-handler-proxy ~netty-version]
                  [io.netty/netty-resolver ~netty-version]
                  [io.netty/netty-resolver-dns ~netty-version]]
-  :profiles {:dev  {:dependencies [[org.clojure/clojure "1.11.1"]
-                                   [criterium "0.4.6"]
-                                   [cheshire "5.10.0"]
-                                   [org.slf4j/slf4j-simple "1.7.30"]
-                                   [com.cognitect/transit-clj "1.0.324"]
-                                   [spootnik/signal "0.2.4"]
-                                   [me.mourjo/dynamic-redef "0.1.0"]]}
+  :profiles {:dev          {:dependencies [[org.clojure/clojure "1.11.1"]
+                                           [criterium "0.4.6"]
+                                           [cheshire "5.10.0"]
+                                           [org.slf4j/slf4j-simple "1.7.30"]
+                                           [com.cognitect/transit-clj "1.0.324"]
+                                           [spootnik/signal "0.2.4"]
+                                           [me.mourjo/dynamic-redef "0.1.0"]
+
+                                           ;; for testing clj-http parity
+                                           [clj-http "3.12.3"]
+                                           [ring/ring-jetty-adapter "1.9.3"]
+                                           [org.apache.logging.log4j/log4j-api "2.17.1"]
+                                           [org.apache.logging.log4j/log4j-core "2.17.1"]
+                                           [org.apache.logging.log4j/log4j-1.2-api "2.17.1"]]}
              :lein-to-deps {:source-paths ["deps"]}
              ;; This is for self-generating certs for testing ONLY:
-             :test {:dependencies [[org.bouncycastle/bcprov-jdk15on "1.69"]
-                                   [org.bouncycastle/bcpkix-jdk15on "1.69"]
-
-                                   ;; for testing clj-http parity
-                                   [clj-http "3.12.3"]
-                                   [ring/ring-jetty-adapter "1.9.3"]
-                                   [org.apache.logging.log4j/log4j-api "2.17.1"]
-                                   [org.apache.logging.log4j/log4j-core "2.17.1"]
-                                   [org.apache.logging.log4j/log4j-1.2-api "2.17.1"]]
-                    :javac-options ^:replace ["--release" "12"] ; necessary for some tests
-                    :jvm-opts ["-Dorg.slf4j.simpleLogger.defaultLogLevel=off"]}}
-  :codox {:src-dir-uri "https://github.com/ztellman/aleph/tree/master/"
+             :test         {:dependencies  [[org.bouncycastle/bcprov-jdk15on "1.69"]
+                                            [org.bouncycastle/bcpkix-jdk15on "1.69"]]
+                            :javac-options ^:replace ["--release" "12"] ; necessary for some tests
+                            :jvm-opts      ["-Dorg.slf4j.simpleLogger.defaultLogLevel=off"]}}
+  :codox {:src-dir-uri               "https://github.com/ztellman/aleph/tree/master/"
           :src-linenum-anchor-prefix "L"
-          :defaults {:doc/format :markdown}
-          :include [aleph.tcp
-                    aleph.udp
-                    aleph.http
-                    aleph.flow]
-          :output-dir "doc"}
+          :defaults                  {:doc/format :markdown}
+          :include                   [aleph.tcp
+                                      aleph.udp
+                                      aleph.http
+                                      aleph.flow]
+          :output-dir                "doc"}
   :plugins [[lein-codox "0.10.7"]
             [lein-marginalia "0.9.1"]
             [lein-pprint "1.3.2"]
             [ztellman/lein-cljfmt "0.1.10"]]
   :java-source-paths ["src/aleph/utils"]
   :cljfmt {:indents {#".*" [[:inner 0]]}}
-  :test-selectors {:default #(not
-                               (some #{:benchmark :stress :integration}
-                                 (cons (:tag %) (keys %))))
-                   :benchmark :benchmark
+  :test-selectors {:default     #(not
+                                   (some #{:benchmark :stress :integration}
+                                     (cons (:tag %) (keys %))))
+                   :benchmark   :benchmark
                    :integration :integration
-                   :stress :stress
-                   :all (constantly true)}
+                   :stress      :stress
+                   :all         (constantly true)}
   :jvm-opts ^:replace ["-server"
                        "-Xmx2g"
                        "-XX:+HeapDumpOnOutOfMemoryError"
