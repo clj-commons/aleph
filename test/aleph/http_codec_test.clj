53516f0a278e7faea3dfe25043e357513568036b
Arnaud Geiser <arnaudgeiser@gmail.com>
2023-01-31 22:04:21 +0100

    fixup! coerce-fn

--- test/aleph/http_codec_test.clj
+++ test/aleph/http_codec_test.clj
@@ -1,13 +1,14 @@
 (ns aleph.http-codec-test
   (:require [aleph.http :as http]
+            [aleph.netty :as netty]
             [aleph.tcp :as tcp]
-            [clojure.test :as t]
+            [clojure.test :refer [deftest testing is]]
             [manifold.stream :as s])
   (:import
    (aleph.protobuf Schema$Person)
    (io.netty.channel ChannelPipeline)
-   (io.netty.handler.codec LengthFieldPrepender)
-   (io.netty.handler.codec.protobuf ProtobufEncoder)))
+   (io.netty.handler.codec.protobuf ProtobufEncoder)
+   (java.lang AutoCloseable)))
 
 (defn- make-person [name]
   (-> (Schema$Person/newBuilder)
@@ -19,20 +20,15 @@
              "protobuf-decoder"
              (ProtobufEncoder.)))
 
-(defn handler [s _]
+(defn tcp-handler [s _]
   (s/put! s (make-person "Doe"))
   (s/close! s))
 
-(def server (tcp/start-server #'handler {:port 9999
-                                         :pipeline-transform add-protobuf-encoder}))
-
-(Schema$Person/parseFrom)
-
-(defn call []
-  (let [s @(tcp/client {:host "localhost"
-                        :port 9999})]
-    (prn (Schema$Person/parseFrom @(s/take! s)))))
-
-(call)
-
-(.close server)
+(deftest protobuf
+  (testing "TCP server"
+    (with-open [server ^AutoCloseable (tcp/start-server #'tcp-handler {:port 0
+                                                                       :coerce-fn identity
+                                                                       :pipeline-transform add-protobuf-encoder})]
+      (let [s @(tcp/client {:host "localhost"
+                            :port (netty/port server)})]
+        (is (= "Doe" (.getName (Schema$Person/parseFrom ^bytes @(s/take! s)))))))))
