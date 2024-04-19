11e9eefdf86a9a060a1e7113dd0b9157aea0a849
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2024-04-19 16:21:29 +0200

    fixup! Introduce utils for making error propagation more self-describing

--- src/aleph/util.clj
+++ src/aleph/util.clj
@@ -6,15 +6,15 @@
   (d/on-realized d identity f))
 
 (defn propagate-error
-  "Registers an error callback with d which will attempt to propagate the error to destination.
+  "Registers an error callback with source which will attempt to propagate the error to destination.
 
   If the error was propagated (i.e. destination wasn't yet realized), on-propagate is invoked with
   the error value.
 
-  Returns d."
-  ([d destination]
-   (propagate-error d destination identity))
-  ([d destination on-propagate]
-   (on-error d (fn [e]
-                 (when (d/error! destination e)
-                   (on-propagate e))))))
+  Returns source."
+  ([source destination]
+   (propagate-error source destination identity))
+  ([source destination on-propagate]
+   (on-error source (fn [e]
+                      (when (d/error! destination e)
+                        (on-propagate e))))))
