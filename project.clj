cb0a22d75ee65a7e8f888dfe61d8799a7819df56
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2024-05-30 11:17:14 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;; you'll need to run the script at `deps/lein-to-deps` after changing any dependencies
-(def netty-version "4.1.108.Final")
+(def netty-version "4.1.110.Final")
 (def brotli-version "1.16.0")
 
 
@@ -7,9 +7,9 @@
   :description "A framework for asynchronous communication"
   :url "https://github.com/clj-commons/aleph"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure "1.11.2"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
                  [org.clojure/tools.logging "1.3.0" :exclusions [org.clojure/clojure]]
-                 [manifold "0.4.2" :exclusions [org.clojure/tools.logging]]
+                 [manifold "0.4.3" :exclusions [org.clojure/tools.logging]]
                  [org.clj-commons/byte-streams "0.3.4"]
                  [org.clj-commons/dirigiste "1.0.4"]
                  [org.clj-commons/primitive-math "1.0.1"]
@@ -27,15 +27,15 @@
                  [io.netty/netty-handler-proxy ~netty-version]
                  [io.netty/netty-resolver ~netty-version]
                  [io.netty/netty-resolver-dns ~netty-version]
-                 [metosin/malli "0.15.0" :exclusions [org.clojure/clojure]]]
+                 [metosin/malli "0.16.1" :exclusions [org.clojure/clojure]]]
   :profiles {:dev                 {:dependencies [[criterium "0.4.6"]
                                                   [cheshire "5.13.0"]
-                                                  [org.slf4j/slf4j-simple "2.0.12"]
+                                                  [org.slf4j/slf4j-simple "2.0.13"]
                                                   [com.cognitect/transit-clj "1.0.333"]
                                                   [spootnik/signal "0.2.5"]
                                                   ;; This is for dev and testing ONLY, not recommended for prod
-                                                  [org.bouncycastle/bcprov-jdk18on "1.77"]
-                                                  [org.bouncycastle/bcpkix-jdk18on "1.77"]
+                                                  [org.bouncycastle/bcprov-jdk18on "1.78.1"]
+                                                  [org.bouncycastle/bcpkix-jdk18on "1.78.1"]
                                                   ;;[org.bouncycastle/bctls-jdk18on "1.75"]
                                                   [io.netty/netty-tcnative-boringssl-static "2.0.65.Final"]
                                                   ;;[com.aayushatharva.brotli4j/all ~brotli-version]
@@ -47,7 +47,7 @@
                                                   [com.aayushatharva.brotli4j/native-osx-aarch64 ~brotli-version]
                                                   [com.aayushatharva.brotli4j/native-osx-x86_64 ~brotli-version]
                                                   [com.aayushatharva.brotli4j/native-windows-x86_64 ~brotli-version]
-                                                  [com.github.luben/zstd-jni "1.5.6-2"]]
+                                                  [com.github.luben/zstd-jni "1.5.6-3"]]
                                    :jvm-opts     ["-Dorg.slf4j.simpleLogger.defaultLogLevel=debug"
                                                   "-Dorg.slf4j.simpleLogger.showThreadName=false"
                                                   "-Dorg.slf4j.simpleLogger.showThreadId=true"
@@ -66,7 +66,7 @@
                                          "-Dio.netty.allocator.type=unpooled"]}
              :pedantic            {:pedantic? :abort}
              :trace               {:jvm-opts ["-Dorg.slf4j.simpleLogger.defaultLogLevel=trace"]}
-             :profile             {:dependencies [[com.clojure-goes-fast/clj-async-profiler "1.2.0"]]
+             :profile             {:dependencies [[com.clojure-goes-fast/clj-async-profiler "1.2.2"]]
                                    :jvm-opts     ["-Djdk.attach.allowAttachSelf"]}}
   :java-source-paths ["src-java"]
   :test-selectors {:default   #(not
