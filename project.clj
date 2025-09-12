836628cded5f785bccbcfedc63dd9b16851ff618
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2025-09-12 07:39:47 +0200

    Bump deps

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;; you'll need to run the script at `deps/lein-to-deps` after changing any dependencies
-(def netty-version "4.1.126.Final")
+(def netty-version "4.1.127.Final")
 (def brotli-version "1.18.0")
 
 
@@ -7,7 +7,7 @@
   :description "A framework for asynchronous communication"
   :url "https://github.com/clj-commons/aleph"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure "1.12.1"]
+  :dependencies [[org.clojure/clojure "1.12.2"]
                  [org.clojure/tools.logging "1.3.0" :exclusions [org.clojure/clojure]]
                  [manifold "0.4.3" :exclusions [org.clojure/tools.logging]]
                  [org.clj-commons/byte-streams "0.3.4"]
@@ -29,7 +29,7 @@
                  [io.netty/netty-resolver-dns ~netty-version]
                  [metosin/malli "0.19.1" :exclusions [org.clojure/clojure]]]
   :profiles {:dev                 {:dependencies [[criterium "0.4.6"]
-                                                  [cheshire "6.0.0"]
+                                                  [cheshire "6.1.0"]
                                                   [org.slf4j/slf4j-simple "2.0.17"]
                                                   [com.cognitect/transit-clj "1.0.333"]
                                                   [spootnik/signal "0.2.5"]
@@ -37,7 +37,7 @@
                                                   [org.bouncycastle/bcprov-jdk18on "1.81"]
                                                   [org.bouncycastle/bcpkix-jdk18on "1.81" :exclusions [org.bouncycastle/bcutil-jdk18on]]
                                                   ;;[org.bouncycastle/bctls-jdk18on "1.75"]
-                                                  [io.netty/netty-tcnative-boringssl-static "2.0.72.Final"]
+                                                  [io.netty/netty-tcnative-boringssl-static "2.0.73.Final"]
                                                   ;;[com.aayushatharva.brotli4j/all ~brotli-version]
                                                   [com.aayushatharva.brotli4j/brotli4j ~brotli-version]
                                                   [com.aayushatharva.brotli4j/service ~brotli-version]
@@ -47,7 +47,7 @@
                                                   [com.aayushatharva.brotli4j/native-osx-aarch64 ~brotli-version]
                                                   [com.aayushatharva.brotli4j/native-osx-x86_64 ~brotli-version]
                                                   [com.aayushatharva.brotli4j/native-windows-x86_64 ~brotli-version]
-                                                  [com.github.luben/zstd-jni "1.5.7-3"]]
+                                                  [com.github.luben/zstd-jni "1.5.7-4"]]
                                    :jvm-opts     ["-Dorg.slf4j.simpleLogger.defaultLogLevel=debug"
                                                   "-Dorg.slf4j.simpleLogger.showThreadName=false"
                                                   "-Dorg.slf4j.simpleLogger.showThreadId=true"
